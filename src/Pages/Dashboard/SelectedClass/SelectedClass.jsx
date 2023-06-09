7d84f2091350eafdc73e1fa9293146abbb0cf7ec
rezaul118255 <rezaul118255@gmail.com>
2023-06-10 01:39:35 +0600

    my select item show

--- src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
+++ src/Pages/Dashboard/SelectedClass/SelectedClass.jsx
@@ -1,14 +1,97 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import UseCart from '../../../hooks/UseCart';
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const SelectedClass = () => {
+    const [cart, refetch] = UseCart();
+    // console.log(cart)
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/Carts/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div>
-            <Helmet>
-                <title>Photography/Selected Class</title>
 
-            </Helmet>
-            <h1>this  is selected class</h1>
+            <div className="w-full">
+                <Helmet>
+                    <title>Photography/Selected Class</title>
+
+                </Helmet>
+
+                <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+                    <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                    {/* <h3 className="text-3xl">Total Price: ${total}</h3> */}
+                    <button className="btn btn-warning btn-sm">PAY</button>
+                </div>
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>image</th>
+                                <th>Class Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                cart.map((item, index) => <tr
+                                    key={item._id}
+                                >
+                                    <td>
+                                        {index + 1}
+                                    </td>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item.class_name}
+                                    </td>
+                                    <td className="text-end">${item.course_fee}</td>
+                                    <td>
+                                        <button onClick={() => handleDelete(item)} className="btn btn-ghost bg-gray-500  text-white"><FaTrashAlt></FaTrashAlt></button>
+                                    </td>
+                                </tr>)
+                            }
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,24 @@ import {
 } from "react-router-dom";
 import { router } from './Routers/Routers.jsx';
 import AuthProvider from './Providers/AuthProvider.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <AuthProvider>
-        <div className='max-w-screen-lg mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <div className='max-w-screen-lg mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )
