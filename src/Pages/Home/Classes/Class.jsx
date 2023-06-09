0d23636d5ee4e114f5bc10c11626591b549c261b
rezaul118255 <rezaul118255@gmail.com>
2023-06-09 20:51:22 +0600

    select button add

--- src/Pages/Home/Classes/Class.jsx
+++ src/Pages/Home/Classes/Class.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../Popular/Popular.css'
 import PrivateRoute from '../../../PrivateRoute/PrivateRoute';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../Providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Class = ({ PopularClass }) => {
-    const { class_name, instructor_name, available_seats, course_fee, admitted_students, image } = PopularClass
+    // const { class_name, instructor_name, available_seats, course_fee, admitted_students, image } = PopularClass
+    const { class_name, instructor_name, available_seats, course_fee, admitted_students, image, id } = PopularClass
+    const { user } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+    const handelAddToCart = PopularClass => {
+        console.log(PopularClass)
+        if (user && user.email) {
+            const orderItem = { menuItemId: id, class_name, instructor_name, available_seats, course_fee, image, email: user.email }
+            fetch('http://localhost:5000/Carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(orderItem)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Your class has been saved',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+
+                    }
+                })
+
+
+
+
+        }
+        else {
+            Swal.fire({
+                title: 'Please login to select Class',
+
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                }
+            })
+        }
+    }
+
+
     return (
         <div>
             <div className='card grid-container'>
@@ -14,10 +67,11 @@ const Class = ({ PopularClass }) => {
                 <div>
                     <h4> ClassName:{class_name}</h4>
                     <h4>Instructors Name:{instructor_name}</h4>
+                    <h4>Enroll Student:{admitted_students}</h4>
                 </div>
                 <div className='flex gap-4'>
                     <h1>price: {course_fee}</h1>
-                    <Link><button className='btn btn-primary'>Select</button></Link>
+                    <Link><button onClick={() => handelAddToCart(PopularClass)} className='btn btn-primary'>Select</button></Link>
                 </div>
 
 
