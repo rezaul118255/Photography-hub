0c5a5387a10b78308893c2afac663738897e97f6
rezaul118255 <rezaul118255@gmail.com>
2023-06-13 11:17:07 +0600

    active_route-Add

--- src/Layouts/Dashbord/Dashbord.jsx
+++ src/Layouts/Dashbord/Dashbord.jsx
@@ -2,105 +2,220 @@
 
 
 
+// import React from 'react';
+// import { NavLink, Outlet } from 'react-router-dom';
+// import useAdmin from '../../hooks/useAdmin';
+// import UseInstructor from '../../hooks/UseInstructor';
+// import { Helmet } from 'react-helmet-async';
+
+// const Dashbord = () => {
+//     // const isAdmin = true;
+//     const [isAdmin] = useAdmin()
+//     // const isInstructors = false;
+//     const [isInstructor] = UseInstructor()
+//     // console.log(isInstructor)
+
+//     return (
+//         <div>
+//             <Helmet>
+//                 <title>Photography/Dashboard</title>
+
+//             </Helmet>
+//             <div className="drawer lg:drawer-open">
+//                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+//                 <div className="drawer-content flex flex-col items-center justify-center">
+//                     {/* Page content here */}
+//                     <Outlet></Outlet>
+//                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+//                 </div>
+//                 <div className="drawer-side ">
+//                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+//                     <ul className="menu  bg-[#645c50] p-4 w-80 h-full bg-base-200 text-base-content">
+
+//                         {
+//                             isAdmin &&
+
+//                             <>
+//                                 <li>
+//                                     <NavLink to="dashboard/manageClass">
+//                                         Manage Class
+//                                     </NavLink>
+
+//                                 </li>
+//                                 <li>
+//                                     <NavLink to="dashboard/manageUsers">
+//                                         Manage Users
+//                                     </NavLink>
+
+//                                 </li>
+
+
+//                             </> || isInstructor &&
+//                             <>
+//                                 <li>
+//                                     <NavLink to="dashboard/addClass">
+//                                         Add a  Class
+//                                     </NavLink>
+
+//                                 </li>
+//                                 <li>
+//                                     <NavLink to="dashboard/myClass">
+//                                         My Class
+//                                     </NavLink>
+
+//                                 </li>
+
+
+//                             </> ||
+//                             <>
+//                                 <li>
+//                                     <NavLink to="dashboard/selectedClass">
+//                                         Selected Class
+//                                     </NavLink>
+
+//                                 </li>
+//                                 <li>
+//                                     <NavLink to="dashboard/enrollClass">
+//                                         EnrollClass
+//                                     </NavLink>
+
+//                                 </li>
+
+
+
+//                             </>
+//                         }
+
+
+//                         {/* Sidebar content here */}
+
+
+//                         <div className="divider"></div>
+//                         <li><NavLink to="/"> Home</NavLink> </li>
+//                         <li><NavLink to="/classes"> Class</NavLink></li>
+//                         <li><NavLink to="/instructors">Instructors</NavLink></li>
+
+//                     </ul>
+
+//                 </div>
+//             </div>
+
+//         </div>
+//     );
+// };
+
+// export default Dashbord;
+
+
+
+
+
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import useAdmin from '../../hooks/useAdmin';
 import UseInstructor from '../../hooks/UseInstructor';
 import { Helmet } from 'react-helmet-async';
+import { AiFillHome, AiOutlineUnorderedList, AiOutlineUser, AiOutlinePlus } from 'react-icons/ai';
 
 const Dashbord = () => {
-    // const isAdmin = true;
-    const [isAdmin] = useAdmin()
-    // const isInstructors = false;
-    const [isInstructor] = UseInstructor()
-    // console.log(isInstructor)
+    const [isAdmin] = useAdmin();
+    const [isInstructor] = UseInstructor();
+
+    const menuVariants = {
+        hidden: { opacity: 0, x: -20 },
+        visible: { opacity: 1, x: 0 },
+    };
 
     return (
         <div>
             <Helmet>
                 <title>Photography/Dashboard</title>
-
             </Helmet>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     {/* Page content here */}
-                    <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+                    <Outlet />
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
+                        Open drawer
+                    </label>
                 </div>
-                <div className="drawer-side ">
+                <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu  bg-[#645c50] p-4 w-80 h-full bg-base-200 text-base-content">
-
-                        {
-                            isAdmin &&
-
+                    <ul className="menu bg-[#645c50] p-4 w-80 h-full bg-base-200 text-base-content">
+                        {isAdmin && (
                             <>
-                                <li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/manageClass">
+                                        <AiOutlineUnorderedList className="mr-2" />
                                         Manage Class
                                     </NavLink>
-
-                                </li>
-                                <li>
+                                </motion.li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/manageUsers">
+                                        <AiOutlineUser className="mr-2" />
                                         Manage Users
                                     </NavLink>
-
-                                </li>
-
-
-                            </> || isInstructor &&
+                                </motion.li>
+                            </>
+                        )}
+                        {isInstructor && (
                             <>
-                                <li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/addClass">
-                                        Add a  Class
+                                        <AiOutlinePlus className="mr-2" />
+                                        Add a Class
                                     </NavLink>
-
-                                </li>
-                                <li>
+                                </motion.li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/myClass">
+                                        <AiOutlineUnorderedList className="mr-2" />
                                         My Class
                                     </NavLink>
-
-                                </li>
-
-
-                            </> ||
+                                </motion.li>
+                            </>
+                        )}
+                        {!isAdmin && !isInstructor && (
                             <>
-                                <li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/selectedClass">
+                                        <AiOutlineUnorderedList className="mr-2" />
                                         Selected Class
                                     </NavLink>
-
-                                </li>
-                                <li>
+                                </motion.li>
+                                <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
                                     <NavLink to="dashboard/enrollClass">
-                                        EnrollClass
+                                        <AiOutlineUser className="mr-2" />
+                                        Enroll Class
                                     </NavLink>
-
-                                </li>
-
-
-
+                                </motion.li>
                             </>
-                        }
-
-
-                        {/* Sidebar content here */}
-
+                        )}
 
                         <div className="divider"></div>
-                        <li><NavLink to="/"> Home</NavLink> </li>
-                        <li><NavLink to="/classes"> Class</NavLink></li>
-                        <li><NavLink to="/instructors">Instructors</NavLink></li>
-
+                        <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
+                            <NavLink to="/">
+                                <AiFillHome className="mr-2" />
+                                Home
+                            </NavLink>
+                        </motion.li>
+                        <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
+                            <NavLink to="/classes">
+                                <AiOutlineUnorderedList className="mr-2" />
+                                Class
+                            </NavLink>
+                        </motion.li>
+                        <motion.li variants={menuVariants} whileHover={{ scale: 1.05 }}>
+                            <NavLink to="/instructors">
+                                <AiOutlineUser className="mr-2" />
+                                Instructors
+                            </NavLink>
+                        </motion.li>
                     </ul>
-
                 </div>
             </div>
-
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -45,70 +45,15 @@
 // export default Banner;
 
 
-// import React from 'react';
-// // import { Flicking } from '@egjs/react-flicking';
-// import '@egjs/react-flicking/dist/flicking.css';
-// import Flicking from '@egjs/react-flicking';
-
-// const Banner = () => {
-//     return (
-//         <div className="carousel-wrapper" >
-//             <Flicking className="carousel" circular={true} autoPlay={true} autoPlayInterval={3000}>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/jWSxrHM/gradient-photography-template-design-23-2149738223.jpg" alt="Slide 1" />
-//                 </div>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/fXV35Tw/gradient-photography-template-design-23-2149738227.jpg" alt="Slide 2" />
-//                 </div>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/G0R9mMn/horizontal-sale-banner-template-photographer-career-hobby-23-2150306830.jpg" alt="Slide 3" />
-//                 </div>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/Z1N2dsW/landing-page-photography-workshop-template-23-2148668971.jpg" alt="Slide 4" />
-//                 </div>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/jWSxrHM/gradient-photography-template-design-23-2149738223.jpg" alt="Slide 5" />
-//                 </div>
-//                 <div className="carousel-item">
-//                     <img src="https://i.ibb.co/jWSxrHM/gradient-photography-template-design-23-2149738223.jpg" alt="Slide 6" />
-//                 </div>
-//             </Flicking>
-//         </div>
-//     );
-// };
-
-// export default Banner;
-
-
-
-
-
-
-
-
-
-import React, { useRef, useEffect } from 'react';
-// import { Flicking, AutoPlay } from '@egjs/react-flicking';
+import React from 'react';
+// import { Flicking } from '@egjs/react-flicking';
 import '@egjs/react-flicking/dist/flicking.css';
-import { Flicking, AutoPlay } from '@egjs/react-flicking';
-
+import Flicking from '@egjs/react-flicking';
 
 const Banner = () => {
-    const flickingRef = useRef(null);
-
-    useEffect(() => {
-        const flicking = flickingRef.current.getInstance();
-        const autoplay = new AutoPlay(3000); // Autoplay interval: 3000ms (3 seconds)
-        flicking.addPlugins(autoplay);
-        autoplay.play();
-        return () => {
-            autoplay.destroy();
-        };
-    }, []);
-
     return (
-        <div className="carousel-wrapper" style={{ marginTop: '50px' }}>
-            <Flicking className="carousel" circular={true} ref={flickingRef}>
+        <div className="carousel-wrapper" >
+            <Flicking className="carousel" circular={true} autoPlay={true} autoPlayInterval={3000}>
                 <div className="carousel-item">
                     <img src="https://i.ibb.co/jWSxrHM/gradient-photography-template-design-23-2149738223.jpg" alt="Slide 1" />
                 </div>
@@ -133,3 +78,10 @@ const Banner = () => {
 };
 
 export default Banner;
+
+
+
+
+
+
+
